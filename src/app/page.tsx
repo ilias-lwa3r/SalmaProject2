bea8e1fe2ffdec48f7c815dbf27cdf651503c6e3
Ait Lamine Ilias <aitlaminilias@gmail.com>
2025-01-03 08:21:42 +0100

    dev

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 'use client'
 import styled from 'styled-components';
+import Link from 'next/link';
 export default function Home() {
   return (
         < Div>
           <Intro>
               Welcome <br/>This is a gift from Ilias to someone<br/> very special<br/>Before we start we need to make sure <br/> you're the right person  
           </Intro>
+          <Answer>
+              <Link href='/qst1'>YES</Link> 
+          </Answer>
         </Div>
   );
 }
@@ -14,6 +18,17 @@ export default function Home() {
 const Div = styled.div` 
     background-color: #F5E7DE;
 
+`;
+const Answer = styled.button` 
+    background-color: #FFAAAB;
+    padding: 50px;
+    color: black;
+    border-radius: 50px;
+    margin: 250px;
+    margin-top: 50px;
+    text-align: center;
+    font-family: Apple Chancery, cursive;
+    font-size: 44px;
 `;
 
 const Intro = styled.p` 
